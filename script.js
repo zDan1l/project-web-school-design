f7d1b6594e970af02a12bfe5ea3eb7b03eb4ac6d
Ziyaa Danil M <ziyaadanil37@gmail.com>
2023-02-01 07:38:44 +0700

    update navbar

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const ul = document.querySelector('.menu-navbar nav ul');
 check.addEventListener('click', function(){
     ul.classList.toggle('show-ul')
 })
+
+const down = document.querySelector('#btn-sublist');
+const sublist = document.querySelector('.sub-list');
+
+down.addEventListener('click', function(){
+    sublist.classList.toggle('show-sublist')
+})
